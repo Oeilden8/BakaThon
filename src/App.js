c7b2e039db60102d82ecdf6858d3babc7125f8e0
Fanny <oeilden8@gmail.com>
2022-01-19 20:52:47 +0100

    suggestion correctly mapped, product props passed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Perceuse from "./components/Perceuse/Perceuse";
+import Suggestions from "./components/Suggestions/Suggestions";
 
 const perceuseList = [
   {
@@ -23,19 +24,41 @@ const perceuseList = [
     percageBois: "13mm",
     son: "72 dB",
   },
+  {
+    // j'ai juste changé prix et nom pour le moment
+    product:
+      "Perceuse visseuse sans fil TEENO PSR 21V + 2 batteries lithium + 41 accessoires + gants professionnels - Rouge",
+    refNumber: "ME1442269",
+    prix: 56.0,
+    alimentation: "batterie",
+    batterie: false,
+    utilisation: "Intensive",
+    fonction: "Perçage et vissage",
+    tension: "18V",
+    capacite: "3 Ah",
+    caracteristiques: "Chargeur, Coffret, Réglage de couple",
+    mandrin: "Auto-serrant",
+    couple: "42Nm",
+    diametreMandrin: "13mm",
+    poids: "1,8kg",
+    vitesses: "2",
+    percageMetal: "13mm",
+    percageBois: "13mm",
+    son: "72 dB",
+  },
 ];
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route
-          path="/"
-          element={perceuseList.map((perceuse, index) => (
-            <Perceuse key={index} {...perceuse} />
-          ))}
-        ></Route>
+        {/* je passe que la 1ere perceuse pour la page produit */}
+        <Route path="/" element={<Perceuse perceuse={perceuseList[0]} />} />
       </Routes>
+      {/* je passe toute la liste pour le modal suggestions */}
+      {perceuseList.map((perceuse, index) => (
+        <Suggestions key={index} {...perceuse} />
+      ))}
     </div>
   );
 }
--- src/components/Perceuse/Perceuse.jsx
+++ src/components/Perceuse/Perceuse.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Perceuse(props) {
+function Perceuse({ perceuse }) {
   return (
     <div>
-      <h1>{props.product}</h1>
-      <p>Prix : {props.prix}€</p>
+      <h1>Je suis un produit</h1>
+      <h2>{perceuse.product}</h2>
+      <p>Prix : {perceuse.prix}€</p>
     </div>
   );
 }
