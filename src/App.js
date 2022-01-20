7e0a4b079ed4f69a86e80f2c940cda7d47f6db8a
EnzoFuche <fuche.enzo@gmail.com>
2022-01-20 02:40:35 +0100

    Préparation modale

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
-
+import Suggestion from "./components/Suggestion/suggestion";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Template Front-End</header>
+      {/* <header className="App-header">Template Front-End</header> */}
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/suggestion" element={<Suggestion />}></Route>
       </Routes>
     </div>
   );
